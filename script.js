d034529f07a6c97985120fa12abcb57f84c791be
Sega-hub <162646916+Sega-hub@users.noreply.github.com>
2024-03-08 02:50:57 +0300

    It just a fun
    Creating this shit for 5 minutes to gift this my girl

---
+++ script.js
@@ -0,0 +1,50 @@
+const button1 = document.querySelector(".page1");
+const button2 = document.querySelector(".page2");
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+
+button1.addEventListener("click", (e)=> {
+    text1.style.visibility = "visible";
+    
+})
+
+button2.addEventListener("click", (e)=> {
+    text2.style.visibility = "visible";
+})
+
+///////////////////
+
+const button3 = document.querySelector(".page3");
+const button4 = document.querySelector(".page4");
+const button5 = document.querySelector(".page5");
+const text3 = document.querySelector(".text3");
+const text4 = document.querySelector(".text4");
+const text5 = document.querySelector(".text5");
+
+button3.addEventListener("click", (e)=> {
+    text3.style.visibility = "visible";
+    
+})
+
+button4.addEventListener("click", (e)=> {
+    text4.style.visibility = "visible";
+})
+
+button5.addEventListener("click", (e)=> {
+    text5.style.visibility = "visible";
+})
+/////////
+
+const button6 = document.querySelector(".page6");
+const button7 = document.querySelector(".page7");
+const text6 = document.querySelector(".text6");
+const text7 = document.querySelector(".text7");
+
+button6.addEventListener("click", (e)=> {
+    text6.style.visibility = "visible";
+    
+})
+
+button7.addEventListener("click", (e)=> {
+    text7.style.visibility = "visible";
+})
